7289bfe5534b2e32757f5eb9c196d63edc4a98c4
Mohit Lahoti <engtechno25@gmail.com>
2024-02-29 00:38:07 +0530

    Adding Binding View in MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.androidaiproject"
     compileSdk = 34
 
+    buildFeatures {
+        dataBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.androidaiproject"
         minSdk = 24
